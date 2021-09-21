9e202d37a8d4ac7726b191e155f811df192463c9
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-21 13:20:53 +0530

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,11 @@
+# wapp to read two integers
+# and find ans = n1 / n2
+
+print("welcome")
+n1 = int(input(" enter first integer "))
+n2 = int(input(" enter second integer "))
+ans = n1 / n2
+print("ans = ", ans)
+
+print("bye")
+
